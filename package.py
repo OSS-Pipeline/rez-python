a1fa5064005eccaac117ae2edd6598e76562ab60
Joshua Senouf <jsenouf@MTLws032.mtloutpost.com>
2019-09-09 18:18:43 -0400

    First version, dedicated to version 2.7.16

---
+++ package.py
@@ -0,0 +1,46 @@
+# Based and improved from https://github.com/piratecrew/rez-python
+
+name = "python"
+
+version = "2.7.16"
+
+authors = [
+    "Guido van Rossum"
+]
+
+description = \
+    """
+    The Python programming language.
+    """
+
+requires = [
+    "gcc-6.3.1"
+]
+
+variants = [
+    ["platform-linux"]
+]
+
+tools = [
+    "2to3",
+    "idle",
+    "pydoc",
+    "python",
+    "python2",
+    "python2.7",
+    "python2.7-config",
+    "python2-config",
+    "python-config",
+    "smtpd.py"
+]
+
+#TODO: Use the SHA1 of the archive instead.
+uuid = "python-2.7.16"
+
+def preprocess():
+    env.CFLAGS.append("-fPIC")
+    env.CXXFLAGS.append("-fPIC")
+
+def commands():
+    env.PATH.prepend("{root}/bin")
+    env.LD_LIBRARY_PATH.prepend("{root}/lib")
